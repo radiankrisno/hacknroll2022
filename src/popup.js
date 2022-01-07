01355c5a1cb7e540a78e94584778121aedf5f1ec
radiankrisno <radiankrisno@gmail.com>
2022-01-07 21:45:26 +0800

    Add loader in popup

--- src/popup.js
+++ src/popup.js
@@ -1,7 +1,11 @@
 import { getStatus } from "./utils.js"
 
 const skip = document.getElementById('skip')
+const skipText = document.getElementById('skipText')
+const skipLoader = document.getElementById('skipLoader')
 const generate = document.getElementById('generate')
+const generateText = document.getElementById('generateText')
+const generateLoader = document.getElementById('generateLoader')
 
 skip.addEventListener('click', handleClickSkip)
 generate.addEventListener('click', handleClickGenerate)
@@ -9,14 +13,26 @@ generate.addEventListener('click', handleClickGenerate)
 async function handleClickSkip(event) {
   event.preventDefault();
 
+  skipText.hidden = true
+  skipLoader.hidden = false
+
   await chrome.storage.sync.set({'question_status': 'skipped', 'last_updated': (new Date()).toJSON()})
+
+  skipText.hidden = false
+  skipLoader.hidden = true
 }
 
 async function handleClickGenerate(event) {
   event.preventDefault();
 
+  generateText.hidden = true
+  generateLoader.hidden = false
+
   const status = await getStatus();
   const questions = status.stat_status_pairs;
+
+  generateText.hidden = false
+  generateLoader.hidden = true
 
   await getQuestion(questions);
 }
